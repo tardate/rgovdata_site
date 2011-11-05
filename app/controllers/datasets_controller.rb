96b8b9330795964c8883c265eb18455299fc1e6d
Paul Gallagher <gallagher.paul@gmail.com>
2011-11-05 15:24:23 +0800

    RGovData GH#3: protect against back-end service errors

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -1,7 +1,18 @@
 class DatasetsController < ApplicationController
   before_filter :setup_resource
+  before_filter :setup_records, :only => :table
+
+  protected
+
   def setup_resource
     @dataset = RGovData::Catalog.get(params[:id])
     @dataset.limit = AppConstants.live_explorer_record_limit if AppConstants.live_explorer_record_limit.present?
+  end
+
+  def setup_records
+    @records = @dataset.records
+  rescue
+    # ignore any errors and just return an empty set
+    @records = []
   end
 end
